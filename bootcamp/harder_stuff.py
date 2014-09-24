689010e19baacfd11757a1cf7686b10d043f7606
lmorinishi <leanna.morinishi@gmail.com>
2014-09-24 18:24:02 -0400

    Plot
    Simple plot

--- bootcamp/harder_stuff.py
+++ bootcamp/harder_stuff.py
@@ -1,6 +1,9 @@
 import numpy as np
 from StringIO import StringIO
 from scipy.stats import hypergeom
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
 # coding: utf-8
 
 # this file is for those of you with a little more coding experience. You need to calculate
@@ -10,7 +13,7 @@ from scipy.stats import hypergeom
 # out. But don't forget to help your teammates, and to figure out your perturbation!
 
 
-
+# cold shock, dtt, h2o2, heat shock, hypo-osmotic shock, nitrogen depletion
 
 # inputs:
 # - a list of [(gene name, gene value) ... ]
@@ -27,14 +30,19 @@ from scipy.stats import hypergeom
 test_genes = [('gene1', 1), ('gene2', 2), ('gene3', -1), ('gene4', 0), ('gene5', 1)]
 test_goid = {'goid1': ['gene1', 'gene2'], 'goid2': ['gene3', 'gene4', 'gene5']}
 
+
+# x = num genes in smaller list with goid
+# M = total length of dataset
+# N = num genes in total dataset with goid
+# n = passed number
 def calculate_enrichment(gene_data, go_to_genes, n=100):
 
-    # get top gene list
+    # get top n gene list
     top_sorted_genes = sorted(gene_data, key=lambda tup: tup[1], reverse=True)
     top_genes=top_sorted_genes[:n]
     top_gene_names= list(zip(*top_genes)[0])
 
-    # get bottom gene list
+    # get bottom n gene list
     bot_sorted_genes = sorted(gene_data, key=lambda tup: tup[1])
     bot_genes=bot_sorted_genes[:n]
     bot_gene_names= list(zip(*bot_genes)[0])
@@ -63,15 +71,17 @@ def calculate_enrichment(gene_data, go_to_genes, n=100):
 
     # calculate scores
     for goid in top_score_list:
-        top_score_list[goid][4] = hypergeom.sf(top_score_list[goid][0]-1, top_score_list[goid][1], top_score_list[goid][2], top_score_list[goid][3])
-        if top_score_list[goid][4] < 0.05:
-            positive_enrichment_scores.append((goid, top_score_list[goid][4]))
-        bot_score_list[goid][4] = hypergeom.sf(bot_score_list[goid][0]-1, bot_score_list[goid][1], bot_score_list[goid][2], bot_score_list[goid][3])
-        if bot_score_list[goid][4] < 0.05:
-            negative_enrichment_scores.append((goid, bot_score_list[goid][4]))
+        top_score_list[goid][4] = hypergeom.logsf(top_score_list[goid][0]-1, top_score_list[goid][1], top_score_list[goid][2], top_score_list[goid][3])
+        positive_enrichment_scores.append((goid, top_score_list[goid][4]))
+        bot_score_list[goid][4] = hypergeom.logsf(bot_score_list[goid][0]-1, bot_score_list[goid][1], bot_score_list[goid][2], bot_score_list[goid][3])
+        negative_enrichment_scores.append((goid, bot_score_list[goid][4]))
+            
+    # 
 
+	positive_enrichment_scores_sorted = sorted(positive_enrichment_scores, key=lambda tup: tup[1])
+	negative_enrichment_scores_sorted = sorted(negative_enrichment_scores, key=lambda tup: tup[1])
 
-    return positive_enrichment_scores,negative_enrichment_scores
+    return positive_enrichment_scores_sorted,negative_enrichment_scores_sorted
 
 
 # You can make your website fancier by creating a figure for each experiment
@@ -81,6 +91,7 @@ def calculate_enrichment(gene_data, go_to_genes, n=100):
 
 # uncomment this line to import the module after you've installed it
 import mpld3
+import operator
 
 # input:
 # - a list of [(gene name, gene value) ... ]
@@ -89,10 +100,20 @@ import mpld3
 # - a dictionary, created by the mpld3 module from a figure you've made (see below)
 #
 def plot_experiment(gene_data):
-    mpld3_dict = None
+    gene_data.sort(key=operator.itemgetter(1))
+    filtered_genes = [gene_tuple[0] for gene_tuple in gene_data]
+    filtered_values = [gene_tuple[1] for gene_tuple in gene_data]
+    filtered_values.reverse()
+    filtered_genes.reverse()
+    
+    fig, ax = plt.subplots(1)
+    scatter = ax.scatter(range(len(gene_data)), filtered_values, edgecolor='none', facecolor='#ff69b4')
+    
+    tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=filtered_genes)
+    mpld3.plugins.connect(fig, tooltip)
 
     # When you've made your plot, convert it with the mpld3 library like so:
-    # mpld3_dict = mpld3.fig_to_dict(fig)
+    mpld3_dict = mpld3.fig_to_dict(fig)
 
     return mpld3_dict
 
